fd2e22c9f824ecf4becc721980c7b54937b56009
Santiago Cañu <santisv4@gmail.com>
2024-06-26 02:15:03 -0300

    hola

--- src/routers/paymentsRouter/cardsPay.js
+++ src/routers/paymentsRouter/cardsPay.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import mercadopago from 'mercadopago';
-import { MercadoPagoConfig, Payment } from 'mercadopago';
+import { MercadoPagoConfig, Payment, Preference } from 'mercadopago';
 import { ACCESS_TOKEN_MP } from '../../config/config.js';
+import { generateToken } from '../../utils/cryptografia.js';
+import { saveTransactionWithToken } from '../../services/transactionServices.js';
 
 export const CardsPay = Router();
 
@@ -9,6 +11,42 @@ const client = new MercadoPagoConfig({
     accessToken: ACCESS_TOKEN_MP,
     options: { timeout: 10000, idempotencyKey: 'abc' }
 })
+
+CardsPay.post('/create_preference-cards', async (req, res) => {
+    const carrito = req.body;
+    const externalReference = generateToken();
+    const { cartId } = req.query;
+
+    const preference = new Preference(client);
+    try {
+        const response = await preference.create({
+            items: carrito,
+            payment_methods: {
+                excluded_payment_methods: [],
+                excluded_payment_types: [],
+                installments: 1
+            },
+            back_urls: {
+                success: 'https://tu-sitio.com/success',
+                failure: 'https://tu-sitio.com/failure',
+                pending: 'https://tu-sitio.com/pending'
+            },
+            notification_url: 'https://alfil-digital.onrender.com/api//webhook',
+            external_reference: externalReference
+        });
+
+        if(cartId && externalReference){
+            await saveTransactionWithToken(cartId, externalReference);
+        }
+
+        console.log(response,' resposne cards ')
+  
+        res.status(200).json(response);
+    } catch (error) {
+        console.error('Error al crear la preferencia:', error);
+        res.status(500).send('Error al crear la preferencia');
+    }
+});
 
 CardsPay.post('/process_payment', async (req, res) => {
     try {
