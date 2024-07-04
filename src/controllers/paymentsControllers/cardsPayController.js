9a0c8535c3b8fd07b71188f07a2dcde18a9114e8
Santiago Cañu <santisv4@gmail.com>
2024-07-04 00:21:39 -0300

    fix1

--- src/controllers/paymentsControllers/cardsPayController.js
+++ src/controllers/paymentsControllers/cardsPayController.js
@@ -2,54 +2,46 @@ import { MercadoPagoConfig, Payment, Preference } from 'mercadopago';
 import { ACCESS_TOKEN_MP } from '../../config/config.js';
 import { saveTransactionCart } from '../../services/transactionsCardServices.js';
 
-
 const client = new MercadoPagoConfig({
     accessToken: ACCESS_TOKEN_MP,
-    options: { timeout: 10000 }
+    options: { timeout: 10000, idempotencyKey: 'abc' }
 })
 
 export const proccessPaymentCard = async (req, res) => {
     try {
-        const { token, issuer_id, payment_method_id, transaction_amount, installments, email, docType, docNumber} = req.body;
+        const { token, issuer_id, payment_method_id, installments, transaction_amount, description, payer } = req.body;
         const application = new Payment(client);
         const { cartId } = req.query;
-        const idempotencyKey = req.headers['x-idempotency-key'];
 
-        console.log('cosas 1', req.body, idempotencyKey,'idempotencyKey')
+        console.log('cosas 1', req.body)
 
         const payment_data = {
-            transaction_amount: Number(transaction_amount),
+            transaction_amount,
             token,
-            description: 'Asistencia informatica',
-            installments: Number(installments),
+            description,
+            installments,
             payment_method_id,
             issuer_id,
             payer: {
-                email: email,
+                email: payer.email,
                 identification: {
-                    type: docType,
-                    number: docNumber
+                    type: payer.identification.type,
+                    number: payer.identification.number,
                 }
-            },
-            three_d_secure_mode: 'optional'
+            }
         };
    
 
-        const payment = await application.create({
-            body: payment_data,
-            requestOptions: {
-                idempotencyKey: idempotencyKey // Usar idempotencyKey en la solicitud a Mercado Pago
-            }
-        });
-
+        const payment = await application.create(payment_data);
+        console.log(payment,'payment en cardsPay')
         if(payment.status_detail === 'accredited'){
             await saveTransactionCart(cartId, payment.id, payment.status_detail) 
             // aca habria que hacer todo lo que te pido chat gpt 
         }
 
-        console.log(payment,'payment en cardsPay')
+  
 
-        res.status(201).json(payment);
+        res.json({ status: payment.body.status, status_detail: payment.body.status_detail, id: payment.body.id });
     } catch (error) {
         console.error('Error al procesar el pago:', error);
         res.status(500).send('Error al procesar el pago');
