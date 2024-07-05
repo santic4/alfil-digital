c90c4525493f1253b62db492c0627b532c07b359
Santiago Cañu <santisv4@gmail.com>
2024-07-05 10:09:54 -0300

    m

--- src/routers/paymentsRouter/cardsPay.js
+++ src/routers/paymentsRouter/cardsPay.js
@@ -1,6 +1,13 @@
 import { Router } from 'express'
+
+import { proccessPaymentCard } from '../../controllers/paymentsControllers/cardsPayController.js';
 import { webHookMP } from '../../controllers/paymentsControllers/mercadoPagoController.js';
 
 export const CardsPay = Router();
+
+CardsPay.post('/process_payment', 
+    proccessPaymentCard
+);
+
 
 CardsPay.post('/webhook', webHookMP)
