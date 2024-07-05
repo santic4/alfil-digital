f2acb4a242060066093c02b51ddd8b944a994ce7
Santiago Cañu <santisv4@gmail.com>
2024-07-05 09:43:07 -0300

    finsh

--- src/models/mongoose/transactionSchema.js
+++ src/models/mongoose/transactionSchema.js
@@ -1,10 +1,10 @@
 import { Schema, model } from 'mongoose';
 
 const transactionSchema = new Schema({
-    externalReference: { type: String, required: true, unique: true },
+    externalReference: { type: String, required: true, sparse: true },
     cart: { type: String, required: true },
     status: { type: String, default: 'pending' },
-    payment_id: { type: String, unique: true }
+    payment_id: { type: String, unique: true , sparse: true}
   });
 
 const Transaction = model('Transaction', transactionSchema);
