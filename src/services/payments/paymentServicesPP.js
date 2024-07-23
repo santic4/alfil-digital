22736b0671d7029eaf1ce7771c28ee721d2e1326
Santiago Cañu <santisv4@gmail.com>
2024-07-23 05:35:57 -0300

    fixing4

--- src/services/payments/paymentServicesPP.js
+++ src/services/payments/paymentServicesPP.js
@@ -4,6 +4,10 @@ import { findTransactionByPaymentId, saveTransactionWithToken, updateTransaction
 class PaymentsServicesPP{
     async createOrderPP(currency_selected, amountUSD, emailSend, carrito, externalReference){
         
+        if(!currency_selected || !amountUSD || !emailSend || !carrito || !externalReference){
+            throw new Error('Token no existe')
+        }
+
         const nombresArchivos = carrito.reduce((acc, item) => {
             return acc.concat(item.productID.fileadj);
         }, []);
@@ -13,25 +17,37 @@ class PaymentsServicesPP{
         const approvalUrl = response.data.links.find(link => link.rel === 'approve').href;
         const payment_id = response.data.id;
       
-        if(!approvalUrl || !payment_id){
-          throw new Error('No se puede realizar el pago.')
+        if(approvalUrl && payment_id){
+            await saveTransactionWithToken(emailSend, externalReference, payment_id, nombresArchivos);
+        }else{
+            throw new Error('No se puede realizar el pago.')
         }
-
-        await saveTransactionWithToken(emailSend, externalReference, payment_id, nombresArchivos);
 
         return approvalUrl
     }
 
     async captureOrderPP(token){
+
+        if(!token){
+            throw new Error('Token no existe')
+        }
         
         const response = await paymentsRepositoryPP.createOrderPP(token);
 
         const referenceId = response.data.purchase_units[0].reference_id;
         const paymentId = response.data.id;
-    
-        await updateTransactionStatus(referenceId, response.data.status, paymentId);
 
-        const foundedTransaction = await findTransactionByPaymentId(paymentId)
+        if(referenceId && paymentId && response){
+            await updateTransactionStatus(referenceId, response.data.status, paymentId);
+        }else{
+            throw new Error('No se puede realizar el pago.')
+        }
+    
+        let foundedTransaction
+        if(paymentId){
+            foundedTransaction: await findTransactionByPaymentId(paymentId)
+        }
+        console.log(foundedTransaction,'foundedTransaction')
     
         return foundedTransaction
     }
