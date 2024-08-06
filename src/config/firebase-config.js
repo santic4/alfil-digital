da4acfb71b9fabb9d87d5d74284af04e4e0ecddd
Santiago Cañu <santisv4@gmail.com>
2024-08-06 13:18:09 -0300

    m

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -3,15 +3,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Verificar si las variables de entorno están definidas
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+const storageBucket = process.env.FIREBASE_BUCKET;
+
+if (!projectId || !clientEmail || !privateKey || !storageBucket) {
+  throw new Error('Missing Firebase configuration environment variables.');
+}
+
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  projectId,
+  clientEmail,
+  privateKey,
 };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'alfil-digital.appspot.com',
+  storageBucket,
 });
 
 const bucket = admin.storage().bucket();
