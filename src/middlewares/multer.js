329963199027d64faf740a11611ec211da32097f
Santiago Cañu <santisv4@gmail.com>
2024-08-06 04:50:32 -0300

    dd

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,15 +1,30 @@
 import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Definir __dirname en módulos ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Configuración de Multer para manejar la carga de archivos
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-
-    if (file.fieldname === 'files') {
-      cb(null, './statics/photos'); 
+    // Define el directorio de destino según el campo del archivo
+    let fileDir;
+    if (file.fieldname === 'images') {
+      fileDir = './statics/fileadj';
     } else {
-      cb(null, './statics/photos'); 
+      fileDir = './statics/photos';
     }
+    // Resuelve la ruta absoluta desde el directorio actual
+    const resolvedPath = path.resolve(__dirname, fileDir);
+    
+    // Verifica y crea el directorio si no existe
+    fs.mkdirSync(resolvedPath, { recursive: true });
 
+    // Llama al callback con el directorio de destino
+    cb(null, resolvedPath);
   },
   filename: function (req, file, cb) {
     // Usa el nombre original del archivo
